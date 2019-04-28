f5aa203391cdfa4b10f5453bfc0c7a63a2f3ffdf
DarrenLiou <fytar9963@gmail.com>
2019-04-27 20:56:59 -0700

    add unit_time.out

--- test/unit_time.c
+++ test/unit_time.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define unit() {for(volatile unsigned long i=0;i<1000000UL;i++);}
 #define period(t) {for(int j=0;j<t;j++){unit();}}
-int main(void){
-    period(1000); 
+int main(int argc, char *argv[]){
+    int len = atoi(argv[1]);
+    period(len);
     return 0;
 }
