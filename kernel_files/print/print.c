ce8563eaff3d4531839b26e1afb013778d40071a
hty-ryan <thsu.ryan@gmail.com>
2019-04-30 13:10:11 +0800

    kernel file

--- kernel_files/print/print.c
+++ kernel_files/print/print.c
@@ -5,8 +5,6 @@
 asmlinkage int __x64_sys_printlog(char* tag, int pid, struct timespec stime, struct timespec ftime)
 {
 	printk("[%s] %d %09ld.%09ld %09ld.%09ld\n", tag, pid, stime.tv_sec, stime.tv_nsec, ftime.tv_sec, ftime.tv_nsec);
-	printk("PRINTK project1");	
 
 	return 0;
 }
-
