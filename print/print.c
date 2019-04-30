5e24b05d2a54a7fccfcbd6d6e4deca2c7ee0b6b4
hty-ryan <thsu.ryan@gmail.com>
2019-04-30 13:03:35 +0800

    kernel files

--- print/print.c
+++
@@ -1,9 +0,0 @@
-#include <linux/kernel.h>
-#include <time.h>
-
-asmlinkage void sys_print(char* tag, int pid, struct timespec stime, struct timespec ftime)
-{
-	printk("[%s] %d %09ld.%09ld %09ld.%09ld\n", tag, pid, stime.tv_sec, stime.tv_nsec, ftime.tv_sec, ftime.tv_nsec);
-	return;
-}
-
