edcaa961315e2f46c83077d66631b3d55d693d77
hty-ryan <thsu.ryan@gmail.com>
2019-04-30 13:07:45 +0800

    kernel files

--- kernel_files/print/print.c
+++ kernel_files/print/print.c
@@ -2,10 +2,10 @@
 #include <linux/time.h>
 #include <linux/linkage.h>
 
-asmlinkage long sys_print(char* tag, int pid, struct timespec stime, struct timespec ftime)
+asmlinkage int __x64_sys_printlog(char* tag, int pid, struct timespec stime, struct timespec ftime)
 {
 	printk("[%s] %d %09ld.%09ld %09ld.%09ld\n", tag, pid, stime.tv_sec, stime.tv_nsec, ftime.tv_sec, ftime.tv_nsec);
-	printk("hello project1");	
+	printk("PRINTK project1");	
 
 	return 0;
 }
